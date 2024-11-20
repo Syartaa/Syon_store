13e779ff4a04dc81325dbdab27221498d395150f
Syartaa <syartapajaziti@gmail.com>
2024-11-20 14:36:48 +0100

    created flutter app

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  //constructer private
+  AppTheme._();
+
+  static ThemeData lighTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData();
+}
