5fc20eaeb0e5a0fb0479d416b85998722b75933c
Syartaa <syartapajaziti@gmail.com>
2024-11-26 12:09:27 +0100

    category section

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,15 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:syon_ecommerce_app/common/widgets/appbar/appbar.dart';
+import 'package:syon_ecommerce_app/common/widgets/image_text/vertical_image_text.dart';
 import 'package:syon_ecommerce_app/common/widgets/products/cart/cart_counter_icon.dart';
 import 'package:syon_ecommerce_app/common/widgets/texts/section_heading.dart';
 
 import 'package:syon_ecommerce_app/features/shop/screens/home/widgets/primary_header_container.dart';
 import 'package:syon_ecommerce_app/features/shop/screens/home/widgets/search_conteiner.dart';
 import 'package:syon_ecommerce_app/utils/constants/text_strings.dart';
-import 'package:syon_ecommerce_app/utils/device/device_utility.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -65,28 +64,41 @@ class HomeAppBar extends StatelessWidget {
               )
             ],
           ),
-          SizedBox(
-            height: 32.0,
-          ),
+          SizedBox(height: 32.0),
 
           //serach bar
           SearchContainer(
             text: 'Search in Store',
           ),
 
-          SizedBox(
-            height: 32.0,
-          ),
+          SizedBox(height: 32.0),
 
           Padding(
             padding: EdgeInsets.only(left: 16.0),
             child: Column(
-              children: const [
-                SectionHeading(
+              children: [
+                const SectionHeading(
                   title: 'Popular Categories',
                   showActionButton: false,
                   textColor: Colors.white,
                 ),
+                const SizedBox(height: 16.0),
+
+                //categories
+                SizedBox(
+                  height: 80,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      return VerticalImageText(
+                        image: 'assets/icons/shoes_icon.jpg',
+                        title: 'Shoes Cateogy',
+                      );
+                    },
+                  ),
+                )
               ],
             ),
           ),
